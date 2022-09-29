ecca378e535aacf4ab8ad2d7a8390079fb342c5f
Jeongwoo Kim <113008679+jwokim01@users.noreply.github.com>
2022-09-29 12:33:44 +0900

    Add files via upload

---
+++ kjw_hw01.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Kim Jeongwoo on 9/25/22.
+//
+// SNU-ID: 2020-14798
+//
+// Homework 01
+//
+// the first homewrok is about 'vector' object from the standard template library in the std (standard) namespace.
+// there are simple vector calculation problems and a function calculation problem.
+
+#include "kjw_hw01_test.h"
+
+int main(){
+
+    run_all_test();
+
+    return 0; 
+}
