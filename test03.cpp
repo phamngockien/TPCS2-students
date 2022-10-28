f13ea7fd221ea519fad99c27f97565db75d84ffe
jeongsardine <113874926+jeongsardine@users.noreply.github.com>
2022-10-28 09:47:41 +0900

    Add files via upload

---
+++ test03.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Jeongil Seo on 27/10/22.
+//
+// SNU-ID: 2018-17034
+//
+// Homework 03
+//
+// In this homework we will solve several problems related to vectors,
+// using the vector object from the standard template library (STL)
+// in the std (standard) namespace.
+
+#include "test_example.hpp"
+
+int main(){
+    test_1();
+    test_2();
+    test_3();
+    return 0;
+}
